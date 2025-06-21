e9577ebed83823079038920dfd1f58e1c041c005
Alexandre Costa <costinhacosta2004@gmail.com>
2025-06-21 15:55:28 +0100

    more stats

--- run_stats.py
+++ run_stats.py
@@ -6,9 +6,9 @@ from mountain_car.carSnn import run_experiment as SnnMountCarRE
 from pendulum.pendulumAnn import run_experiment as AnnPendulumRE
 from pendulum.pendulumSnn import run_experiment as SnnPendulumRE
 
-#run_stats(10, 100, "cart/cartAnn_config.txt", "resultsAnnCart", AnnCartRE)
-#run_stats(10, 100, "cart/cartSnn_config.txt", "resultsSnnCart", SnnCartRE)
-#run_stats(3, 100, "mountain_car/mountain_config_ann.txt", "resultsAnnMountainCar", AnnMountCarRE)
-#run_stats(3, 100, "mountain_car/mountain_config_snn.txt", "resultsSnnMountainCar", SnnMountCarRE)
-run_stats(2, 100, "pendulum/pendulum_config_ann.txt", "resultsAnnPendulum", AnnPendulumRE)
-run_stats(1, 100, "pendulum/pendulum_config_snn.txt", "resultsSnnPendulum", SnnPendulumRE)
+#run_stats(30, 100, "cart/cartAnn_config.txt", "resultsAnnCart", AnnCartRE)
+#run_stats(30, 100, "cart/cartSnn_config.txt", "resultsSnnCart", SnnCartRE)
+#run_stats(15, 100, "mountain_car/mountain_config_ann.txt", "resultsAnnMountainCar", AnnMountCarRE)
+#run_stats(15, 100, "mountain_car/mountain_config_snn.txt", "resultsSnnMountainCar", SnnMountCarRE)
+run_stats(15, 100, "pendulum/pendulum_config_ann.txt", "resultsAnnPendulum", AnnPendulumRE)
+run_stats(15, 100, "pendulum/pendulum_config_snn.txt", "resultsSnnPendulum", SnnPendulumRE)
